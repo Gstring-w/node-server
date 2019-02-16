a3b0c3ee0b056f5f4a9e0fe6e9bf5c0d0bce706d
Gstring-w <1327312540@qq.com>
2019-02-16 10:45:10 +0800

    feat: add client web page

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const commentData = require("./web/commentData");
 const homeArticle = require("./web/homeArticle");
 const managementPost = require("./web/managementPost");
 const getUser = require("./web/getUsername");
+const homePage = require('./web/homePage');
 const filter = require("./filter");
 
 const express = require("express");
@@ -13,6 +14,7 @@ const cookie = require("cookie-parser");
 const expressJwt = require("express-jwt");
 const app = express();
 
+app.use(express.static('public'));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.static("public"));
@@ -24,7 +26,7 @@ app.use(
   expressJwt({
     secret: "guan_blog"
   }).unless({
-    path: ["/api/logi"]
+    path: ["/api/login","/api/home/articles","/"]
   })
 );
 app.use(function(err, req, res, next) {
@@ -33,11 +35,12 @@ app.use(function(err, req, res, next) {
   }
 });
 
+app.get('/',homePage)
 // 接口
 // 登录接口
 app.post("/api/login", loginWeb); // http://localhost:12306/api/login
 
-app.get("/api/a", commentData); // 评论接口 http://localhost:12306/api/comment?page=0&count=1&articleId=1
+app.get("/api/comment", commentData); // 评论接口 http://localhost:12306/api/comment?page=0&count=1&articleId=1
 app.get("/api/home/articles", homeArticle); //首页文章接口 http://localhost:12306/api/home/articles?page=0&count=1
 app.get("/api/management/post", managementPost); //获取将要发布文章的接口 http://localhost:12306/api/management/post?page=0&count=1
 
---
+++ public/precache-manifest.5971c56b8c7c1a2554af5182a6c443c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8de7d0e8572e6ccad4d5",
+    "url": "/static/js/main.8de7d0e8.chunk.js"
+  },
+  {
+    "revision": "6eae135065e465be6b67",
+    "url": "/static/js/1.6eae1350.chunk.js"
+  },
+  {
+    "revision": "8de7d0e8572e6ccad4d5",
+    "url": "/static/css/main.0571753b.chunk.css"
+  },
+  {
+    "revision": "6eae135065e465be6b67",
+    "url": "/static/css/1.54102135.chunk.css"
+  },
+  {
+    "revision": "afbb602f704558daaa2e9bc26726ba24",
+    "url": "/index.html"
+  }
+];
